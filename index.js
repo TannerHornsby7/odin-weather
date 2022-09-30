53dee17e024e23e7d6339d8a7d3aba60367eaf45
Tanner Hornsby <tannerhornsby7@gmail.com>
2022-09-30 14:50:12 -0500

    Implement functioning weather search

--- index.js
+++ index.js
@@ -8,4 +8,24 @@ async function getWeather(place) {
     
     throw new Error(response.status);
 }
-getWeather('France').then(weather => alert(weather.weather[0].main));
+
+async function getBackground(weather) {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=d7afc9d7d30ef48de59698fbaef3a3cb`);
+    
+    if(response.status === 200) {
+        const json = await response.json();
+        return json;
+    }
+    
+    throw new Error(response.status);
+}
+
+const input = document.getElementById('loc');
+
+document.getElementById('loc').onkeydown = function(e){
+    console.log('keydown detected')
+    if(e.key == "Enter"){
+        console.log("weather location entered")
+        getWeather(input.value).then(weather => alert(weather.weather[0].main));
+    }
+};
