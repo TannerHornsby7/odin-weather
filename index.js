e757e424890872922935a75fbc58d6799d4b5d3d
Tanner Hornsby <tannerhornsby7@gmail.com>
2022-09-30 20:49:11 -0500

    connect temperature and description dom elements to open weather api

--- index.js
+++ index.js
@@ -21,11 +21,27 @@ async function getBackground(weather) {
 }
 
 const input = document.getElementById('loc');
+const local = document.querySelector('h2.location');
+const temp = document.querySelector('h3.temp');
+const desc = document.querySelector('h4.desc');
 
 document.getElementById('loc').onkeydown = function(e){
     console.log('keydown detected')
     if(e.key == "Enter"){
         console.log("weather location entered")
-        getWeather(input.value).then(weather => alert(weather.weather[0].main));
+        getWeather(input.value).then(weather => {
+            local.textContent = input.value.toUpperCase();
+             temp.textContent = 1.8*(weather.main.temp-273).toFixed(1) + 32;
+             desc.textContent = weather.weather[0].description.toUpperCase();
+            console.log(temp);
+        });
     }
 };
+
+/*
+Circuitry Work Required:
+
+get current temperature => h3.temp
+get current main and description => h4.desc => background image
+
+*/
